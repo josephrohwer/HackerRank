9f951868f8f421982747fc54d13c554bbf7acb64
Joseph Rohwer <joseph.d.rohwer@gmail.com>
2017-06-27 09:18:04 -0500

    Day 27: Testing

---
+++ 30DaysOfCode/day27.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "5" << endl
+		 << "5 3" << endl << "-1 90 999 100 0" << endl
+		 << "4 2" << endl << "0 -1 2 1" << endl
+		 << "3 3" << endl << "-1 0 1" << endl
+		 << "6 1" << endl << "-1 0 1 -1 2 3" << endl
+		 << "7 3" << endl << "-1 0 1 2 3 4 5" << endl;
+}
